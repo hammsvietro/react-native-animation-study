cd9445337be1e875ba1e326e22b3763ee032f935
Pedro Hamms Vietro <hammsvietro@gmail.com>
2020-05-26 16:57:15 -0300

    .

--- src/Deck.js
+++ src/Deck.js
@@ -2,10 +2,22 @@ import React from 'react';
 
 import { View, Text, Animated } from 'react-native';
 
+import { Card, Button, Image } from 'react-native-elements'
+
 export default function Deck({data}) {
   
   const renderCards = () => {
-    return data.map((item) => <Text key={item.id}>{item.text}</Text>)
+    return data.map((item) => (
+      <Card
+        key={item.id}
+        title={item.text}
+      >
+        <Image source={{uri: item.uri}} resizeMode="cover" style={{width: 200, height: 200}} />
+        <Text style={{ marginBottom: 10}}>OVO OVO OVO</Text>
+        <Button icon={{name: 'code'}} backgroundColor="#03a9f4" title="View Now!" />
+
+      </Card>
+    ))
   };
 
   return(
